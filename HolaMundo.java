08ded47b3bf32af25f54a1cf93b161357f97daa1
albertommartin <alberto.m.martin@accenture.com>
2014-04-28 13:31:28 +0200

    commit num. 3
    añadido mas codigo para ver como cambia

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,14 @@
 public class HolaMundo{
 	public static void main (String args[])
 	{
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo3333333333333");
+		System.out.println("frase 2 ");
+		System.out.println("frase 3 ");
+	}
+	public static void main2 (String args[])
+	{
+		System.out.println("Hola Mundo222222223333333333333");
+		System.out.println("frase 22 ");
+		System.out.println("frase 33 ");
 	}
 }
